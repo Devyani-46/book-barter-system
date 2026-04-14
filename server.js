e1b34ef578a614596780d235d883bf2b8e11b523
Devyani-46 <patildev2142@gmail.com>
2026-04-14 18:15:29 +0530

    Fix MongoDB connection error

--- server.js
+++ server.js
@@ -23,11 +23,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use("/api/admin", adminAuth);
 
 /* ================= MONGODB ================= */
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => {
     console.error("❌ Mongo Error:", err);
